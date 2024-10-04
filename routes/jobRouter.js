0e5a2ae251e66615897f586f2262503c32afd76d
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-10-04 09:04:04 +0300

    V2

---
+++ routes/jobRouter.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  getAllJobs,
+  getJobById,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobControllers");
+const { requireAuth } = require("../middleware/requireAuth");
+
+const router = express.Router();
+
+router.get("/", getAllJobs);
+router.get("/:jobId", getJobById);
+
+// Protected routes
+router.post("/", requireAuth, createJob);  
+router.put("/:jobId", requireAuth, updateJob); 
+router.delete("/:jobId", requireAuth, deleteJob);  
+
+module.exports = router;
